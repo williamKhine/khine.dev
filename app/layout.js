bea88b1e8d3a8675830fb34e9cc351379ed2352a
williamKhine <williamkhine@outlook.com>
2024-01-21 16:10:07 -0500

    Update description

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "William Khine",
-  description: "William's portfolio website",
+  description: "Fullstack Developer in Toronto, Canada",
 };
 
 export default function RootLayout({ children }) {
