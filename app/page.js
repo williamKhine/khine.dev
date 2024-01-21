685f2d66412898594caa591fb974ba447538b202
williamKhine <williamkhine@outlook.com>
2024-01-21 17:45:41 -0500

    Add social links

--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    "khine.dev under construction"
+    <main>
+      <h1 className="text-xl">khine.dev still under construction...</h1>
+      <div className="flex gap-x-3">
+        <a target="_blank" href="mailto:william@khine.dev">Email</a>
+        <a target="_blank" href="https://github.com/williamKhine">GitHub</a>
+        <a target="_blank" href="https://www.linkedin.com/in/williamkhine">LinkedIn</a>
+      </div>
+    </main>
   );
 }
