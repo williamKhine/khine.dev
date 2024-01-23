261581648129a060eb088bf76cdc3f34af1b78a8
williamKhine <williamkhine@outlook.com>
2024-01-22 22:41:55 -0500

    Presentable under construction page

--- app/page.js
+++ app/page.js
@@ -1,17 +1,19 @@
 export default function Home() {
   return (
-    <main className="m-4">
-      <h1 className="text-xl">khine.dev still under construction...</h1>
-      <div className="flex gap-x-3">
-        <a target="_blank" href="mailto:william@khine.dev">
-          Email
-        </a>
-        <a target="_blank" href="https://github.com/williamKhine">
-          GitHub
-        </a>
-        <a target="_blank" href="https://www.linkedin.com/in/williamkhine">
-          LinkedIn
-        </a>
+    <main className="h-[calc(100dvh-88px)] flex items-center justify-center">
+      <div className="">
+        <h1 className="text-xl">khine.dev still under construction...</h1>
+        <div className="flex gap-x-3">
+          <a target="_blank" href="mailto:william@khine.dev">
+            Email
+          </a>
+          <a target="_blank" href="https://github.com/williamKhine">
+            GitHub
+          </a>
+          <a target="_blank" href="https://www.linkedin.com/in/williamkhine">
+            LinkedIn
+          </a>
+        </div>
       </div>
     </main>
   );
