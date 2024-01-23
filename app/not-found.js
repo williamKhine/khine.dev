22381bf88c213a7870f012c122eccc54bcfd32cc
williamKhine <williamkhine@outlook.com>
2024-01-22 22:33:12 -0500

    Not found page

---
+++ app/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-[calc(100dvh-88px)] items-center justify-center">
+      <h1 className="text-center text-xl">404 - Page Not Found</h1>
+    </div>
+  );
+}
