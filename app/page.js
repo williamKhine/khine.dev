692007dbbb608c1c93941223409f8fecb8c5d440
williamKhine <williamkhine@outlook.com>
2024-01-22 22:33:56 -0500

    Format homepage

--- app/page.js
+++ app/page.js
@@ -3,9 +3,15 @@ export default function Home() {
     <main className="m-4">
       <h1 className="text-xl">khine.dev still under construction...</h1>
       <div className="flex gap-x-3">
-        <a target="_blank" href="mailto:william@khine.dev">Email</a>
-        <a target="_blank" href="https://github.com/williamKhine">GitHub</a>
-        <a target="_blank" href="https://www.linkedin.com/in/williamkhine">LinkedIn</a>
+        <a target="_blank" href="mailto:william@khine.dev">
+          Email
+        </a>
+        <a target="_blank" href="https://github.com/williamKhine">
+          GitHub
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/williamkhine">
+          LinkedIn
+        </a>
       </div>
     </main>
   );
